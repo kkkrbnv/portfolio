12fadb17d4d32754deb6a59ba07562cd0f1db2fe
kkkrbnv <kurbanov2899@mail.ru>
2026-05-17 02:38:12 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://arslankurbanov.com',
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind()],
 });
